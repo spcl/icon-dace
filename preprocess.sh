62d8c1de90e87146b8c186cada93a705808f71db
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2025-01-29 23:34:38 +0100

    A preprocessor script that works (but depends on some external libraries that are quite difficult to even find).

---
+++ preprocess.sh
@@ -0,0 +1,26 @@
+INCS="-I./src/include -I./externals/ecrad/include -I./externals/ecrad/radiation -I./externals/yaxt/include -I./externals/comin/include -I./externals/ppm/include/f77"
+
+# For ecrad.
+for f in $(find externals/ecrad | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+for f in $(find externals/rte-rrtmgp | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+
+# For velocity tendencies.
+for f in $(find src | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+for f in $(find externals/comin/src | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+for f in $(find externals/fortran-support/src | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+for f in $(find externals/mtime/src | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
+for f in $(find externals/ppm/src | grep -E "\.F90\$|\.f90\$"); do
+  gfortran $INCS -cpp -E -P "${f}" > "${f}.tmp" && mv "${f}.tmp" "${f}" && sed -i '' '/./,$!d' "${f}"
+done
