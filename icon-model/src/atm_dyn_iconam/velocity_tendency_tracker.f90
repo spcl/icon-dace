62566c45bf22f3c4ab0c45440709483fa40540ce
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-05-12 18:04:27 +0200

    Tracker

--- icon-model/src/atm_dyn_iconam/velocity_tendency_tracker.f90
+++ icon-model/src/atm_dyn_iconam/velocity_tendency_tracker.f90
@@ -1,4 +1,7 @@
 MODULE velocity_tendency_tracker
     IMPLICIT NONE
-    INTEGER :: velocity_tendencies_call_count = 0
+    INTEGER :: velocity_tendencies_c1_count = 0
+    INTEGER :: velocity_tendencies_c2_count = 0
+    INTEGER :: velocity_tendencies_c3_count = 0
+    INTEGER :: velocity_tendencies_c4_count = 0
   END MODULE velocity_tendency_tracker
